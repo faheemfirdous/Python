5c5a559615c33eea32f0459565e59573b1927fcb
Faheem Firdous <faheemfirdous77@gmail.com>
2023-09-16 17:38:35 +0530

    Update main.py

--- Nato alphabet/main.py
+++ Nato alphabet/main.py
@@ -1,7 +1,21 @@
 import pandas
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
-phonetic_dic = {row.letter: row.code for (index, row) in data.iterrows()}
-word = input("Enter thew word: ").upper()
-output_list = [phonetic_dic[letter] for letter in word]
 
-print(output_list)
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+phonetic_dic = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dic)
+
+
+def generate_phonetic():
+    word = input("Enter thew word: ").upper()
+
+    try:
+        output_list = [phonetic_dic[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please!!")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
